280a33924a560a7229455a18d2ebe0e38be03a08
Jackson <jhhemme43@gmail.com>
2022-01-13 11:53:52 -0700

    mvp

---
+++ Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  shooping list
+//
+//  Created by Jackson Hemme on 1/13/22.
+//
+
+import Foundation
+class Item{
+    var name : String
+    init(name: String)
+    {
+        self.name = name
+    }
+}
+
--- shooping list/ViewController.swift
+++ shooping list/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController, UITableViewDataSource
+{
+    @IBOutlet weak var tableView1: UITableView!
+    var items:[Item] = []
+    override func viewDidLoad()
+    {
+            tableView1.dataSource = self
+        let item1 = Item(name: "Milk")
+        let item2 = Item(name: "Eggs")
+        items = [item1, item2]
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return items.count 
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView1.dequeueReusableCell(withIdentifier: "myCell", for :indexPath); cell.textLabel?.text = items[indexPath.row].name
+        return cell
+    }
+    
 }
-
